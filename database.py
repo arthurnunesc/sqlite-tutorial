518fb3ee6b10644190c70fe65ec6297ab8960f3b
Arthur Nunes <arthurnunesc@proton.me>
2023-01-30 22:16:43 +0100

    Course done

--- database.py
+++ database.py
@@ -2,25 +2,78 @@ import sqlite3
 
 
 def print_full_table_per_row():
+    # Connect to database and create a cursor
+    connection = sqlite3.connect("customer.db")
+    c = connection.cursor()
+    # Querying the database to get everythin
     c.execute("SELECT rowid, * FROM customers")
     items = c.fetchall()
-    print("NAME \t\t EMAIL")
+    # Printing everything
     for item in items:
-        print(f"{item[0]} {item[1]}:\t {item[2]}")
+        print(item)
+    # Commit our command and close our connection
+    connection.commit()
+    connection.close()
 
 
-# Connect to database
-# connection = sqlite3.connect(":memory:") # Used to store things in memory without creating a database
-connection = sqlite3.connect("customer.db")
+# Add a new record to the table
+def add_one_record(first_name, last_name, email):
+    connection = sqlite3.connect("customer.db")
+    c = connection.cursor()
+
+    c.execute("INSERT INTO customers VALUES (?, ?, ?)", (first_name, last_name, email))
+
+    connection.commit()
+    connection.close()
 
 
-# Create a cursor
-c = connection.cursor()
+def add_many_records(list):
+    connection = sqlite3.connect("customer.db")
+    c = connection.cursor()
+
+    c.executemany("INSERT INTO customers VALUES (?, ?, ?)", list)
+
+    connection.commit()
+    connection.close()
 
 
-# Adding data to the database
-# First command
-# c.execute("""--sql 
+# Deletes the records using rowid as String
+def delete_record(id):
+    connection = sqlite3.connect("customer.db")
+    c = connection.cursor()
+
+    c.execute("DELETE FROM customers WHERE rowid = (?)", (id,))
+
+    connection.commit()
+    connection.close()
+
+
+def lookup_with_email(email):
+    connection = sqlite3.connect("customer.db")
+    c = connection.cursor()
+
+    c.execute("SELECT rowid, * FROM customers WHERE email = (?)", (email,))
+
+    items = c.fetchall()
+    for item in items:
+        print(item)
+
+    connection.commit()
+    connection.close()
+
+
+# # Connect to database
+# # connection = sqlite3.connect(":memory:") # Used to store things in memory without creating a database
+# connection = sqlite3.connect("customer.db")
+
+
+# # Create a cursor
+# c = connection.cursor()
+
+
+# # Adding data to the database
+# # First command
+# c.execute("""--sql
 # CREATE TABLE customers (
 #     first_name text,
 #     last_name text,
@@ -50,13 +103,10 @@ c = connection.cursor()
 # many_customers = [
 #     ("Wes", "Brown", "wes@brown.com"),
 #     ("Steph", "Kuewa", "steph@kuewa.com"),
-#     ("Dan", "Pas", "dan@pas.com")
+#     ("Dan", "Pas", "dan@pas.com"),
 # ]
 
-# c.executemany("""--sql
-# INSERT INTO customers
-# VALUES (?, ?, ?);
-# """, many_customers)
+# c.executemany("INSERT INTO customers VALUES (?, ?, ?)", many_customers)
 
 
 # # Datatypes:
@@ -67,7 +117,7 @@ c = connection.cursor()
 # BLOB (Images, songs, etc. Random shit basically)
 
 
-# # Uptading records
+# # Updating records
 # c.execute("""--sql
 # UPDATE customers
 # SET first_name = 'Bobby'
@@ -75,7 +125,7 @@ c = connection.cursor()
 # """)
 # print_full_table_per_row()
 
-# Better update
+# # Better update
 # c.execute("""--sql
 # UPDATE customers
 # SET first_name = 'John'
@@ -100,10 +150,14 @@ c = connection.cursor()
 # print_full_table_per_row()
 
 
-# Deleting data
+# # Deleting data
 # c.execute("DELETE FROM customers ") # Deletes EVERYTHING
 # c.execute("DELETE FROM customers WHERE rowid = 6")
 # print_full_table_per_row()
+
+
+# # Dropping a table
+# c.execute("DROP TABLE customers")
 
 
 # # Querying the database
@@ -135,23 +189,35 @@ c = connection.cursor()
 #     print(f"{item[0]} {item[1]}:\t {item[2]}")
 
 
-# Querying the database - ORDER BY
+# # Querying the database - ORDER BY
 # c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC") # ASC is the default
 
-c.execute("SELECT rowid, * FROM customers ORDER BY last_name DESC")
-
-items = c.fetchall()
-for item in items:
-    print(item)
-items = c.fetchall()
-for item in items:
-    print(item)
+# c.execute("SELECT rowid, * FROM customers ORDER BY last_name DESC")
 
 
+# # Querying the database - AND/OR
+# c.execute(
+#     "SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' AND rowid = 3"
+# )  # Only Mary
+# c.execute(
+#     "SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' OR rowid = 5"
+# )  # Mary, Wes and Steph
 
-# Commit our command
-connection.commit()
+
+# # Querying the database - Limiting Results
+# c.execute("SELECT rowid, * FROM customers LIMIT 2")  # rowid 1 and 2
+# c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC LIMIT 2")  # rowid 5 and 4
 
 
-# Close our connection
-connection.close()
+# # Printing what we got
+# items = c.fetchall()
+# for item in items:
+#     print(item)
+
+
+# # Commit our command
+# connection.commit()
+
+
+# # Close our connection
+# connection.close()
---
+++ our_app.py
@@ -0,0 +1,18 @@
+import database
+
+# database.print_full_table_per_row()
+
+# database.add_one_record("Jonas", "Esticado", "pequenojonas@gmail.com")
+
+# database.delete_record("9")
+
+# example_list = [
+#     ("Brenda", "Smitherton", "brenda@smitherton.com"),
+#     ("Joshua", "Raintree", "joshua@raintree.com"),
+# ]
+
+# database.add_many_records(example_list)
+
+database.lookup_with_email("brenda@smitherton.com")
+
+# database.print_full_table_per_row()
